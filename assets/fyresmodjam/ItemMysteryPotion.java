01316ab6029ffc4e571f29195277530872051847
Kenneth Dignam <kennethdignam@hotmail.com>
2013-08-02 17:08:36 -0400

    More Potion work. Can't remember if I commited. :P

--- assets/fyresmodjam/ItemMysteryPotion.java
+++ assets/fyresmodjam/ItemMysteryPotion.java
@@ -6,10 +6,15 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.Icon;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 public class ItemMysteryPotion extends Item {
 
@@ -40,6 +45,29 @@ public class ItemMysteryPotion extends Item {
 	
 	public String getItemDisplayName(ItemStack par1ItemStack) {
         return "Mystery Potion #" + (par1ItemStack.getItemDamage() + 1);
+    }
+	
+	public EnumAction getItemUseAction(ItemStack par1ItemStack) {
+        return EnumAction.drink;
+    }
+	
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+        par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+        return par1ItemStack;
+    }
+	
+	public int getMaxItemUseDuration(ItemStack par1ItemStack) {
+        return 32;
+    }
+	
+	public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+        if (!par3EntityPlayer.capabilities.isCreativeMode) {--par1ItemStack.stackSize;}
+        par3EntityPlayer.addPotionEffect(new PotionEffect(UnmarkedPotionData.potionValues[par1ItemStack.getItemDamage()], 100, 1, true));
+        return par1ItemStack;
+    }
+	
+	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
+        return false;
     }
 
 }
