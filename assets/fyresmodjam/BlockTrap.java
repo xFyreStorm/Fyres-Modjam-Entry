fe93d34a1a91e8a7bfe4789c876ae19b42b4f07f
Kenneth Dignam <kennethdignam@hotmail.com>
2013-08-04 18:03:59 -0400

    Back. Traps now explode if world type = Explosive Traps, and the block below is destroyed

--- assets/fyresmodjam/BlockTrap.java
+++ assets/fyresmodjam/BlockTrap.java
@@ -114,8 +114,9 @@ public class BlockTrap extends BlockContainer
     public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5) {
     	super.onNeighborBlockChange(par1World, par2, par3, par4, par5);
     	
-    	if (!par1World.isBlockSolidOnSide(par2, par3 -1, par4, ForgeDirection.SOUTH, true)) {
+    	if(!par1World.isRemote && !par1World.isBlockSolidOnSide(par2, par3 -1, par4, ForgeDirection.SOUTH, true)) {
     		par1World.setBlockToAir(par2, par3, par4);
+    		if(CommonTickHandler.worldData.currentDisadvantage.equals("Explosive Traps")) {par1World.createExplosion(null, par2 + 0.5F, par3 + 0.5F, par4 + 0.5F, 1.33F, true);}
     	}
     }
     
