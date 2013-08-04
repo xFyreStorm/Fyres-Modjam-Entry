d24cc76411c2371a1db31c56598587c6c5f1e866
Kenneth Dignam <kennethdignam@hotmail.com>
2013-08-04 18:48:07 -0400

    Still tweaking worldgen

--- assets/fyresmodjam/PillarGen.java
+++ assets/fyresmodjam/PillarGen.java
@@ -14,23 +14,21 @@ public class PillarGen implements IWorldGenerator {
         if(world.provider.dimensionId == 0 && random.nextInt(25) == 0) {
         	boolean placed = false;
         	
-        	for(int y = 30, added = 0; y < 254 && !placed && added < 4; y++) {
+        	for(int y = 127, added = 0; y > 30 && !placed && added < 4; y--) {
         		for(int x = chunkX * 16; x < chunkX * 16 + 16 && !placed && added < 4; x++) {
         			for(int z = chunkZ * 16; z < chunkZ * 16 + 16 && !placed && added < 4; z++) {
-        				if(random.nextInt(10) != 0 || world.isAirBlock(x, y, z) || Block.blocksList[world.getBlockId(x, y, z)].isBlockReplaceable(world, x, y, z)) {continue;}
+        				if(random.nextInt(10) != 0 || world.isAirBlock(x, y, z) || Block.blocksList[world.getBlockId(x, y, z)].isBlockReplaceable(world, x, y, z) || world.getBlockId(x, y, z) == ModjamMod.blockTrap.blockID) {continue;}
         				
         				Block block = ModjamMod.blockPillar;
         		        
         		        if(block.canPlaceBlockAt(world, x, y + 1, z)) {
-        		        	world.setBlock(x, y + 1, z, block.blockID);
-        		        	world.setBlockMetadataWithNotify(x, y + 1, z, 0, 0);
+        		        	world.setBlock(x, y + 1, z, block.blockID, 0, 0);
         		            
-        		        	world.setBlock(x, y + 2, z, block.blockID);
-        		        	world.setBlockMetadataWithNotify(x, y + 2, z, 1, 0);
+        		        	world.setBlock(x, y + 2, z, block.blockID, 1, 0);
         		        	
         		        	placed = random.nextBoolean();
         		        	
-        		        	y += 10;
+        		        	y -= 10;
         		        	added++;
         		        	
         		        	//System.out.println(x + ", " + y + ", " + z);
